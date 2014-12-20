34bec5e78712c1ca91463726f2510c636936d338
Prachetas Prabhu <prachetas.github@gmail.com>
2014-12-20 06:50:56 +0000

    Adds ability to control number of search results and fixes Method too large error

--- src/twitter_test/core.clj
+++ src/twitter_test/core.clj
@@ -1,7 +1,7 @@
 (ns twitter-test.core
   (:require [twitter-test.creds :as cr]
             [clojure.java.io :as io]
-            [clojure.string :as str]
+            [clojure.string :as st]
             [twitter.oauth :as o]
             [twitter.callbacks :as c]
             [twitter.callbacks.handlers :as h]
@@ -10,7 +10,11 @@
   (:import java.io.FileWriter
            java.io.StringWriter))
 
+;; (take 50 (map :word (word-freq (get-words (from-file)))))
+
 (def creds (apply o/make-oauth-creds cr/oauth-creds))
+
+(def res-file "/tmp/twitter_result.edn")
 
 #_(defn to-file [f input]
   (let [sw (StringWriter.)]
@@ -18,13 +22,13 @@
       (clojure.pprint/pprint (f input) sw)
       (.write w (.toString sw)))))
 
-(defn to-file [f input]
+(defn to-file [input & {:keys [file] :or {file res-file}}]
   (let [sw (StringWriter.)]
-    (clojure.pprint/pprint (f input) sw)
+    (clojure.pprint/pprint input sw)
     (spit "/tmp/twitter_result.edn" (.toString sw))))
 
-(defn from-file [& {:keys [file] :or {file "/tmp/twitter_result.edn"}}]
-  (eval (read-string (slurp file))))
+(defn from-file [& {:keys [file] :or {file res-file}}]
+  (read-string (slurp file)))
 
 (defn make-call [f params]
   (f :oauth-creds creds :params params))
@@ -32,14 +36,14 @@
 (defn show-user [username]
   (make-call r/users-show {:screen-name username}))
 
-(defn search [term]
-  (make-call s/search {:q term}))
+(defn search [term & {:keys [count] :or {count "25"}}]
+  (make-call s/search {:q term :count count}))
 
 (defn tweet-texts [data]
   (map :text (get-in data [:body :statuses])))
 
 (defn get-words [data]
-  (mapcat #(str/split % #" ") (tweet-texts data)))
+  (mapcat #(st/split % #" ") (tweet-texts data)))
 
 (defn lexical-diversity [words]
   (/ (* 1.0 (count (into #{} words))) (count words)))
